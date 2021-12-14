b962aae671ce67104f3122e2eb5d7d0708c10d6a
konscodes <katashi.rain@gmail.com>
2021-12-14 15:31:35 +0900

    day8 part1 completed

--- 08/day8.py
+++ 08/day8.py
@@ -1,7 +1,24 @@
-with open('test8.txt') as file_object:
-    data = file_object.read()
-    data = [i.strip(' |') for i in data.split(sep='\n')]
-    input_data = data[0::2]
-    output_data = data[1::2]
-    print(input_data)
-    print(output_data)
+# Part1
+from pathlib import Path
+
+path = Path(__file__).resolve()
+parent = path.parent
+file_path = parent / 'day8.txt'
+
+def read_data(file_path):
+    with open(file_path) as file_object:
+        data = file_object.read()
+        data = [i.split(' | ') for i in data.split(sep='\n')]
+        input_data = [i[0] for i in data]
+        output_data = [i[1] for i in data]
+        print(output_data)
+    return input_data, output_data
+
+
+input_data, output_data = read_data(file_path)
+counter = 0
+for line in output_data:
+    segment_list = line.split(' ')
+    for segment in segment_list:
+        counter += 1 if len(segment) <= 4 or len(segment) == 7 else 0
+print(counter)
