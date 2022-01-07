f3cf7625c84f101f27417e249ecf12a8148651e8
konscodes <katashi.rain@gmail.com>
2022-01-07 23:03:26 +0900

    part1 rewritten with neighbors check

---
+++ 09/part1.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+
+def is_valid_position(data, i, j):
+    return i >= 0 and i < len(data) and \
+           j >= 0 and j < len(data[0])
+
+
+def neighbors(data, i, j):
+    return [(i+x, j+y) for x, y in [(-1, 0), (1, 0), (0, -1), (0, 1)]
+                       if is_valid_position(data, i+x, j+y)]
+
+
+def low_point(data, i, j):
+    return all(data[i][j] < data[x][y] 
+                for x,y in neighbors(data, i, j))
+
+
+def low_points(data):
+    return [data[i][j] 
+            for i in range(len(data))
+            for j in range(len(data[0]))
+            if low_point(data, i, j)]
+
+
+def risk_levels(low_points):
+    return [int(i) + 1 for i in low_points]
+
+
+def total_risk(data):
+    return sum(risk_levels(low_points(data)))
+
+
+if __name__ == '__main__':
+    path = Path(__file__).resolve()
+    file = path.parent / 'day9.txt'
+
+    with open(file) as f:
+        data = f.read().splitlines()
+    
+    print('part1: ', total_risk(data))
