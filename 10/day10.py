58e8ca394c42fb165a9f4ff0c1bf0057b3c299bd
konscodes <katashi.rain@gmail.com>
2022-01-18 12:53:26 +0900

    part1 completed

--- 10/day10.py
+++ 10/day10.py
@@ -1,11 +1,22 @@
 from pathlib import Path
 
+def corrupted(line, open):
+    for symb in line:
+        if symb in symbols.keys():
+            open.append(symb)
+        elif symb in symbols.values():
+            if symbols[open[-1]] == symb:
+                open.pop()
+            else:
+                return symb
+    
+
 def syntax_check(data):
-    return [')']
+    return [corrupted(line, list()) for line in data]
 
 
 def total_syntax_error(data):
-    return sum([syntax_check(data).count(symbol)*points for symbol, points in close_symbol])
+    return sum([syntax_check(data).count(char)*points for char, points in points.items()])
 
 
 if __name__ == '__main__':
@@ -15,7 +26,6 @@ if __name__ == '__main__':
     with open(file) as f:
         data = f.read().splitlines()
     
-    open_symbol = [')',']','}','>']
-    #close_symbol = {')': 3,']': 57,'}': 1197,'>': 25137}
-    close_symbol = [(')',3), (']',57), ('}',1197), ('>',25137)]
+    symbols = {'(':')', '[':']', '{':'}', '<':'>'}
+    points = {')':3, ']':57, '}':1197, '>':25137}
     print('part1: ', total_syntax_error(data))
