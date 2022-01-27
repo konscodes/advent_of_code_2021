2672f926e5795b4ad66885aa144a1b415323beb9
konscodes <katashi.rain@gmail.com>
2022-01-27 21:19:32 +0900

    day11 set the func structure

--- 11/day11.py
+++ 11/day11.py
@@ -1,15 +1,33 @@
 from pathlib import Path
 
+def outbreak(flashes, neighbors):
+    # loop through each neighbor [(x,y), (x,y)]
+    # for each (x,y)
+    #   check if 9 ->>
+    #       set to 0
+    #       increment flashes[0] += 1
+    #       call outbreak(flashes, neighbors(i,j))
+    #   else increment data[x][y]
+    return 0
+
+
 def energy_levels(data):
-    return {'modified': [[1], [2,3,1]], 'flashes': 10}
+    flashes = [0]
+    # loop through grid indexes i,j
+    # for each i,j 
+    #   check if 9 ->> 
+    #       set to 0
+    #       increment flashes[0] += 1
+    #       call outbreak(flashes, neighbors(i,j))
+    #   else increment data[i][j]
+    return flashes[0]
 
 
 def count_flashes(data):
     total = 0
-    for i in range(100):
-        iteration = energy_levels(data)
-        total += iteration['flashes']
-        data = iteration['modified']
+    for i in range(1):
+        print(i)
+        total += energy_levels(data)
     return total
         
 
@@ -19,5 +37,6 @@ if __name__ == '__main__':
 
     with open(file) as f:
         data = f.read().splitlines()
+        data = [[int(y) for y in x] for x in data]
     
     print('part1: ', count_flashes(data))
