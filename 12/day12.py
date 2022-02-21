c9d620112a8476adfe58fdf99c620243eeb05b1d
konscodes <katashi.rain@gmail.com>
2022-02-21 18:32:34 +0900

    completed locate func for a single path

--- 12/day12.py
+++ 12/day12.py
@@ -1,15 +1,14 @@
 from pathlib import Path
 
 def locate_path(entry, caves, path):
-    if entry == 'end':
-        return path
     for cave in caves[entry]:
         if cave in path and cave[0].islower():
             continue
         else:
             path.append(cave)
-            locate_path(cave, caves, path)
+            return path if cave == 'end' else locate_path(cave, caves, path)
     return path
+# Locate path works for a single path. How to find all possible paths?
 
 def locate_all_pathways(caves):
     return [['List of pathways in a list']]
@@ -23,12 +22,20 @@ def read_data(file):
         data = [tuple(x.split('-')) for x in data]
     return data
 
-def cave_connections(data):
-    return {'Each cave': ['And its connection']}
+def cave_connections(data, caves):
+    for pair in data:
+        if pair[0] not in caves:
+            caves[pair[0]] = []
+        caves[pair[0]].append(pair[1])
+        if pair[1] not in caves:
+            caves[pair[1]] = []
+        caves[pair[1]].append(pair[0])
+    return caves
 
 if __name__ == '__main__':
     path = Path(__file__).resolve()
     file = path.parent / 'test12.txt'
     pairs = read_data(file)
-    caves = cave_connections(pairs)
+    caves = cave_connections(pairs, {})
+    print(locate_path('start', caves, ['start']))
     print('part1: ', locate_all_pathways(caves))
